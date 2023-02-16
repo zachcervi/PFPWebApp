1e67449e9ab16452e29c26e5d5ef497b907c3f95
Zach Cervi <zachcervi@gmail.com>
2023-02-15 17:24:18 -0700

    Updated API call

--- src/api/visit.js
+++ src/api/visit.js
@@ -2,15 +2,17 @@ import axios from "axios";
 
 function signUpForVisit(data) {
   if (data.firstName && data.roomNumber) {
-    const { firstName, roomNumber } = data;
+    const config = {
+      headers: {
+        Authorization:
+          "Bearer " + process.env.REACT_APP_PFP_WEB_APP_ACCESS_TOKEN,
+      },
+    };
+    const url =
+      "https://us-central1-pawsforpatients-16f0a.cloudfunctions.net/app/visits";
+
     return axios
-      .post(
-        "https://us-central1-pawsforpatients-16f0a.cloudfunctions.net/app/visit",
-        {
-          firstName,
-          roomNumber,
-        }
-      )
+      .post(url, data, config)
       .then((response) => response)
       .catch((error) => error);
   }
